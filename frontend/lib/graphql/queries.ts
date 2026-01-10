6fc44fca52ade90e2e423dba7ea1ab1c13265f85
Hirotoshi Uchida <uho02741358@gmail.com>
2026-01-10 15:39:44 +0900

    Create queries.ts

---
+++ frontend/lib/graphql/queries.ts
@@ -0,0 +1,21 @@
+export const ALL_POSTS_QUERY = `
+  query AllPosts {
+    posts {
+      nodes {
+        slug
+      }
+    }
+  }
+`;
+
+export const POST_QUERY = `
+  query Post($slug: ID!) {
+    post(id: $slug, idType: SLUG) {
+      title
+      blocks {
+        name
+        renderedHtml
+      }
+    }
+  }
+`;
