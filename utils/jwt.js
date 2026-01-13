7c1000be054d671f8da2ff3b268af9489f9dff4f
Hirotoshi Uchida <uho02741358@gmail.com>
2026-01-13 16:51:37 +0900

    Update jwt.js

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const JWT_SECRET = process.env.JWT_SECRET || 'your-secret-key-change-this-in-production';
+const JWT_SECRET = process.env.JWT_SECRET || '59283ddab4ac691dfa8be21b11d3fb9b2bbb80d8d1703da1a971dc78937bceed04dfe4ee651565eacaaf0cd5ca6365a54035fc2a18868d9da465159289cb1a1a';
 const JWT_EXPIRES_IN = '24h';
 
 const generateToken = (payload) => {
